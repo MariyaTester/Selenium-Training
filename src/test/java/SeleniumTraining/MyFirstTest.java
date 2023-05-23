6edf58e939dcde2fe3a66bfc117ee1c1759d0ca2
Mariya Fokina <mariyafokina1997@gmail.com>
2023-05-23 19:00:47 +0400

    Синхронизация созданного проекта; Commit первого работающего автотеста (открытие google chrome, выполнение поиска, закрытие браузера)

---
+++ src/test/java/SeleniumTraining/MyFirstTest.java
@@ -0,0 +1,36 @@
+package SeleniumTraining;
+
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+import static org.openqa.selenium.support.ui.ExpectedConditions.titleIs;
+
+public class MyFirstTest {
+    private WebDriver driver;
+    private WebDriverWait wait;
+
+    @BeforeEach
+    public void start() {
+        driver = new ChromeDriver();
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+    }
+
+    @Test
+    public void myFirstTest() {
+        driver.get("https://www.google.com/");
+        driver.findElement(By.name("q")).sendKeys("WebDriver\n");
+        wait.until(titleIs("WebDriver - Поиск в Google"));
+    }
+
+    @AfterEach
+    public void stop() {
+        driver.quit();
+        driver = null;
+    }
+
+}
